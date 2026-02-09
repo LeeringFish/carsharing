3cbd0d6225928405a65edacb2bfc14695da43ccb
LeeringFish <theleeringfish@proton.me>
2026-02-08 21:21:56 -0500

    add UserInterface.java

---
+++ UserInterface.java
@@ -0,0 +1,10 @@
+package carsharing;
+
+public class UserInterface {
+
+    private void printMainMenu() {
+        System.out.println("1. Log in as a manager");
+        System.out.println("0. Exit");
+    }
+
+}
