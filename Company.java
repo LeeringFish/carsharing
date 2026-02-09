4f3d5514dca68a3153a146db2b78fde758185ba2
LeeringFish <theleeringfish@proton.me>
2026-02-09 15:47:45 -0500

    add Company.java, CompanyDao.java

---
+++ Company.java
@@ -0,0 +1,20 @@
+package carsharing;
+
+public class Company {
+    private int id;
+    private String name;
+
+    public Company(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+}
